8e5ba23d5c5d510d44b6a4b6d2cb83b7ecd621ea
azwri <aazwri@gmail.com>
2020-10-31 02:46:06 +0300

    create frontend and backen and react app

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+import List from './components/List';
+import Add from './components/Add';
+import Update from './components/Update';
+import {Switch, Route, Link} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="container">
+
+      <Link to='/'>List</Link> |
+      <Link to='/add'>Add</Link> |
+      <Link to='/update/1'>Update</Link> |
+
+      <Switch>
+        <Route path='/' component={List} exact/>
+        <Route path='/add' component={Add}/>
+        <Route path='/update/:id' component={Update}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Update.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Update() {
+    return (
+        <div className='container'>
+            <h1>Upadate</h1>
+        </div>
+    )
+}
+
+export default Update;
